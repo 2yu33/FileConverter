cb409806b0b3593a35f22f1b8a1a42cfde2eea8c
liang <2863079308@qq.com>
2023-05-20 20:26:18 +0800

    完成csv文件读取并下载转换后的文件

--- src/main/java/com/wu/ming/controller/CsvController.java
+++ src/main/java/com/wu/ming/controller/CsvController.java
@@ -6,16 +6,20 @@ import com.opencsv.exceptions.CsvValidationException;
 import com.wu.ming.service.CsvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 @RestController
 public class CsvController {
+
     @Autowired
     private CsvService csvService;
     @PostMapping("/csv2json")
@@ -30,18 +34,38 @@ public class CsvController {
     public String csvToYaml(@RequestBody String csvString) throws IOException, CsvValidationException {
         return csvService.csvToYaml(csvString);
     }
-    @PostMapping(value = "/file/csv2json", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String csvToJson(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
+    @PostMapping(value = "/file/csv2json")
+    public ResponseEntity<byte[]> fileCsvToJson(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
         return csvService.fileCsvToJson(file);
     }
-    @PostMapping(value = "/file/csv2xml", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String csvToXml(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
+    @PostMapping(value = "/file/csv2xml")
+    public ResponseEntity<byte[]> fileCsvToXml(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
         return csvService.fileCsvToXml(file);
     }
-    @PostMapping(value = "/file/csv2yaml", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String csvToYaml(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
+    @PostMapping(value = "/file/csv2yaml")
+    public ResponseEntity<byte[]> fileCsvToYaml(@RequestPart("file") MultipartFile file) throws IOException, CsvValidationException {
         return csvService.fileCsvToYaml(file);
     }
+    @PostMapping(value = "/download")
+    public ResponseEntity<byte[]> downloadFile(@RequestBody String  requestData) throws IOException, CsvValidationException {
+        // 将请求中的数据转换为字节数组
+        byte[] fileData = requestData.getBytes();
 
+        // 指定下载文件的名称和类型
+        String fileName = "file.json";
+        String contentType = MediaType.TEXT_PLAIN_VALUE;
+
+        // 创建临时文件
+        File tempFile = File.createTempFile("temp", null);
+        try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+            outputStream.write(fileData);
+        }
+
+        // 设置下载响应头
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(org.apache.commons.io.FileUtils.readFileToByteArray(tempFile));
+    }
 }
 
--- src/main/java/com/wu/ming/service/CsvService.java
+++ src/main/java/com/wu/ming/service/CsvService.java
@@ -1,6 +1,7 @@
 package com.wu.ming.service;
 
 import com.opencsv.exceptions.CsvValidationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -10,7 +11,7 @@ public interface CsvService {
     String csvToJson(String csvString) throws IOException, CsvValidationException;
     String csvToXml(String csvString) throws IOException, CsvValidationException;
     String csvToYaml(String csvString) throws IOException, CsvValidationException;
-    String fileCsvToJson(MultipartFile file) throws IOException, CsvValidationException;
-    String fileCsvToXml(MultipartFile file) throws IOException, CsvValidationException;
-    String fileCsvToYaml(MultipartFile file) throws IOException, CsvValidationException;
+    ResponseEntity<byte[]> fileCsvToJson(MultipartFile file) throws IOException, CsvValidationException;
+    ResponseEntity<byte[]> fileCsvToXml(MultipartFile file) throws IOException, CsvValidationException;
+    ResponseEntity<byte[]> fileCsvToYaml(MultipartFile file) throws IOException, CsvValidationException;
 }
