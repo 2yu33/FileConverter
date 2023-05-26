db5fa7c7461e9a64df04b158263c18610fbb6d9f
ChengHong <2320842611@qq.com>
2023-05-26 15:23:28 +0800

    完善yaml转csv

--- src/main/java/com/wu/ming/service/YAMLService.java
+++ src/main/java/com/wu/ming/service/YAMLService.java
@@ -1,7 +1,6 @@
 package com.wu.ming.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,7 +29,7 @@ public interface YAMLService {
      * @return 转换后的字符串
      * yaml转换csv
      */
-    String toCSV(String yamlString) throws JsonProcessingException;
+    String toCSV(String yamlString) throws IOException;
 
     /**
      * @author: 小C
--- src/main/java/com/wu/ming/service/impl/YAMLServiceImpl.java
+++ src/main/java/com/wu/ming/service/impl/YAMLServiceImpl.java
@@ -6,9 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import com.wu.ming.service.YAMLService;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,9 +13,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.*;
-import java.nio.charset.Charset;
-import java.util.Arrays;
+import java.io.*;;
 import java.util.List;
 import java.util.Map;
 
@@ -78,39 +73,32 @@ public class YAMLServiceImpl implements YAMLService {
      * @author: 小C
      */
     @Override
-    public String toCSV(String yamlString) {
-        try {
-            Yaml yaml = new Yaml();
-            yaml.load(yamlString);
-        } catch (Exception e) {
-            return JSON.toJSONString("输入yaml格式错误");
-        }
-        // 创建YAMLMapper和ObjectMapper实例
-        YAMLMapper yamlMapper = new YAMLMapper();
-        Object yamlObject = null;
-        try {
-            yamlObject = yamlMapper.readValue(yamlString, Object.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        Map<String, Object> yamlMap = (Map<String, Object>) yamlObject;
+    public String toCSV(String yamlString){
+        Yaml yaml = new Yaml();
+
+        // 解析 YAML 字符串
+        List<Map<String, Object>> data = yaml.load(yamlString);
+
+        // 创建 StringBuilder 以构建 CSV 字符串
         StringBuilder csvBuilder = new StringBuilder();
-        for (Map.Entry<String, Object> entry : yamlMap.entrySet()) {
-            csvBuilder.append(entry.getKey()).append(",");
-            if (entry.getValue() instanceof Map) {
-                Map<String, String> mapValue = (Map<String, String>) entry.getValue();
-                for (Map.Entry<String, String> mapEntry : mapValue.entrySet()) {
-                    csvBuilder.append(mapEntry.getValue()).append(",");
-                }
-            } else {
-                csvBuilder.append(entry.getValue().toString()).append(",");
+
+        // 写入 CSV 标题行
+        Map<String, Object> firstItem = data.get(0);
+        for (String key : firstItem.keySet()) {
+            csvBuilder.append(key).append(",");
+        }
+        csvBuilder.deleteCharAt(csvBuilder.length() - 1);
+        csvBuilder.append("\n");
+
+        // 写入 CSV 数据行
+        for (Map<String, Object> item : data) {
+            for (Object value : item.values()) {
+                csvBuilder.append(value).append(",");
             }
-            csvBuilder.setLength(csvBuilder.length() - 1);
+            csvBuilder.deleteCharAt(csvBuilder.length() - 1);
             csvBuilder.append("\n");
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_TYPE, "text/csv");
         return csvBuilder.toString();
     }
 
@@ -220,41 +208,44 @@ public class YAMLServiceImpl implements YAMLService {
      */
     @Override
     public ResponseEntity<byte[]> fileYamlToCsv(MultipartFile file) throws IOException {
+        //读取文件
+        BufferedReader reader = null;
+        StringBuffer buffer = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+            String line = reader.readLine();
+            while (StringUtils.hasLength(line)) {
+                buffer.append(line);
+                buffer.append("\n");
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
 
-        // 读取 YAML 文件的内容
-        byte[] yamlBytes = file.getBytes();
-        String yamlString = new String(yamlBytes);
-        Yaml yaml = new Yaml();
-        Map<String, Object> obj = yaml.load(yamlString);
-
-        // 创建 CSV printer
-        StringWriter csvWriter = new StringWriter();
-        CSVPrinter csvPrinter = new CSVPrinter(csvWriter, CSVFormat.RFC4180);
-
-
-        // 将每个数据行写入 CSV 文件
-        for (Map.Entry<String, Object> entry : obj.entrySet()) {
-            List<String> rowData = Arrays.asList(
-                    entry.getKey(),
-                    entry.getValue().toString(),
-                    ""
-            );
-
-            csvPrinter.printRecord(rowData);
         }
-
-        // 清理并返回 CSV 文件
-        csvPrinter.flush();
-        byte[] csvBytes = csvWriter.toString().getBytes(Charset.forName("UTF-8"));
+        // 将请求中的数据转换为字节数组
+        byte[] fileData = this.toCSV(buffer.toString()).getBytes();
+        // 指定下载文件的名称和类型
         String fileName = "file.csv";
+
         // 创建临时文件
         File tempFile = File.createTempFile("temp", null);
         try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
-            outputStream.write(csvBytes);
+            outputStream.write(fileData);
         }
+        // 设置下载响应头
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType("text/csv"))
                 .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
                 .body(org.apache.commons.io.FileUtils.readFileToByteArray(tempFile));
     }
+
 }
