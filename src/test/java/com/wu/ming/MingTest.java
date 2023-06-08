3508c955d6f32454deeb732e26f11a1c68050b8e
liang <2863079308@qq.com>
2023-06-08 08:31:00 +0800

    完成MongoDB数据库的读与存。。。

--- src/test/java/com/wu/ming/MingTest.java
+++ src/test/java/com/wu/ming/MingTest.java
@@ -2,9 +2,11 @@ package com.wu.ming;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 public class MingTest {
+    public static void main(String[] args) {
 
-
-
+    }
 }
