7858c6af6dc7b9182509fae217056144eea5149f
Mhare <1940307627@qq.com>
2023-06-09 16:46:48 +0800

    完成elasticSearch的插入，分页查询功能

--- src/test/java/com/wu/ming/MingTest.java
+++ src/test/java/com/wu/ming/MingTest.java
@@ -1,5 +1,9 @@
 package com.wu.ming;
+import com.wu.ming.dao.FileEsDao;
+import com.wu.ming.pojo.FileSearchDTO;
 import com.wu.ming.service.JsonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
@@ -11,6 +15,19 @@ public class MingTest {
     @Resource
     JsonService jsonService;
 
+    @Autowired
+    private FileEsDao fileEsDao;
 
+
+    @Test
+    public void testEs(){
+        FileSearchDTO fileSearchDTO = FileSearchDTO
+                .builder()
+                .fileName("测试文件名称")
+                .content("测试文件数据的内容")
+                .fileSuffix(".xml")
+                .build();
+        fileEsDao.save(fileSearchDTO);
+    }
 
 }
