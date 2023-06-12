09159169f5e24139e61ccafefdb8f358380611bf
yu <yu@qq.com>
2023-06-12 15:42:51 +0800

    Json2 Achievement Really now

---
+++ src/main/java/com/wu/ming/mapper/ConverterMapper.java
@@ -0,0 +1,20 @@
+package com.wu.ming.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wu.ming.model.Converter;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author 余某某
+* @description 针对表【converter】的数据库操作Mapper
+* @createDate 2023-06-08 14:16:05
+* @Entity com.wu.ming.model.Converter
+*/
+@Mapper
+public interface ConverterMapper extends BaseMapper<Converter> {
+
+}
+
+
+
+
---
+++ src/main/java/com/wu/ming/service/ConverterService.java
@@ -0,0 +1,41 @@
+package com.wu.ming.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wu.ming.model.Converter;
+import com.wu.ming.utils.PageUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+* @author 余某某
+* @description 针对表【converter】的数据库操作Service
+* @createDate 2023-06-08 14:16:05
+*/
+public interface ConverterService extends IService<Converter> {
+      /**
+       * 前端上传文件，接受文件并存储文件的内容
+       * @param file
+       * @return
+       */
+      long addConverter( MultipartFile file);
+
+      /**
+       * 通过id查找并删除相应的内容
+       * @param id
+       * @return
+       */
+      boolean removeConverter(int id);
+
+      /**
+       * 通过id查询单个内容
+       * @param id
+       * @return
+       */
+      Converter selectConverter(int id);
+
+      /**
+       * 查询所有结果并分页展示
+       * @return
+       */
+      Page<Converter> selectConverters(PageUtils pageUtils);
+}
