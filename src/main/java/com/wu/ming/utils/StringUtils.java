1113cbc53537fa05eaf4e04cf8b7190576b1945d
Mhare <1940307627@qq.com>
2023-06-14 16:55:55 +0800

    实现对xml转换的校验，实现xml格式化的功能

---
+++ src/main/java/com/wu/ming/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.wu.ming.utils;
+
+public class StringUtils {
+
+    //字符串判空
+    public static boolean isEmpty(String s) {
+        if (s == null || "".equals(s)) {
+            return true;
+        }
+        return false;
+    }
+}
