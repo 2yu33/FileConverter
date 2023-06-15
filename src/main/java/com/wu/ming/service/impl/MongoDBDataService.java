47e38779be5aa151b36a031ac220528180e5df20
liang <2863079308@qq.com>
2023-06-15 22:02:12 +0800

    完善Mongodb增删改查...

--- src/main/java/com/wu/ming/service/impl/MongoDBDataService.java
+++ src/main/java/com/wu/ming/service/impl/MongoDBDataService.java
@@ -1,13 +1,22 @@
 package com.wu.ming.service.impl;
 
+import com.opencsv.exceptions.CsvValidationException;
+import com.wu.ming.common.ErrorCode;
 import com.wu.ming.dao.MongoDBDataRepository;
+import com.wu.ming.exception.BusinessException;
 import com.wu.ming.pojo.MongoDBData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -57,5 +66,26 @@ public class MongoDBDataService {
         mongoTemplate.remove(query, MongoDBData.class);
     }
 
-    // ...
+    public ResponseEntity downloadFile(String fileName) throws IOException, CsvValidationException {
+        Query query = new Query(Criteria.where("_id").is(fileName));
+        String data = mongoTemplate.findOne(query, MongoDBData.class).getData();
+        // 将请求中的数据转换为字节数组
+        byte[] fileData = data.getBytes();
+
+        // 指定下载文件的名称和类型
+        String contentType = MediaType.TEXT_PLAIN_VALUE;
+
+        // 创建临时文件
+        File tempFile = File.createTempFile("temp", null);
+        try (FileOutputStream outputStream = new FileOutputStream(tempFile)) {
+            outputStream.write(fileData);
+        }
+        // 设置下载响应头
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header("Content-Disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(org.apache.commons.io.FileUtils.readFileToByteArray(tempFile));
+
+    }
+
 }
